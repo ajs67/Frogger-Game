fb120d97ce2b28d4ed92af9f76960b475ec95959
Alexander Schwartz <ajs67@buffalo.edu>
2025-03-09 22:28:08 -0700

    Initial commit.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import * as THREE from "three";
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { player } from "./compenents/Player";
+import "./style.css";
+
+const scene = new THREE.Scene();
+scene.add(player);
+
+const camera = Camera();
+scene.add(camera);
+
+const renderer = Renderer();
+renderer.render(scene, camera);

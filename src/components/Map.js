eef592f2e04345c1600e086b61f6a5930fb781e1
Alexander Schwartz <ajs67@buffalo.edu>
2025-03-15 23:15:20 -0700

    Added additional grass rows before the initial row.

--- src/components/Map.js
+++ src/components/Map.js
@@ -31,8 +31,10 @@ export const metadata = [
 export const map = new THREE.Group();
 
 export function initializeMap() {
-    const grass = Grass(0);
-    map.add(grass);
+    for (let rowIndex = 0; rowIndex > -5; rowIndex--) {
+        const grass = Grass(rowIndex);
+        map.add(grass);
+    }
     addRows();
 }
 

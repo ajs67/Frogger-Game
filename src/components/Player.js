348070fea5d0baba1283ac3ad6e270ecd198ccc0
Alexander Schwartz <ajs67@buffalo.edu>
2025-03-17 00:33:00 -0700

    Added constraints to limit player movement off map.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { endsUpInValidPosition } from "../utilities/endsUpInValidPosition";
 
 export const player = Player();
 
@@ -40,6 +41,16 @@ export const position = {
 export const movesQueue = [];
 
 export function queueMove(direction) {
+    const isValidMove = endsUpInValidPosition(
+        {
+            rowIndex: position.currentRow,
+            tileIndex: position.currentTile,
+        },
+        [...movesQueue, direction]
+    );
+
+    if (!isValidMove) return;
+
     movesQueue.push(direction);
 }
 
---
+++ src/utilities/endsUpInValidPosition.js
@@ -0,0 +1,32 @@
+import { calculateFinalPosition } from "./calculateFinalPosition";
+import { minTileIndex, maxTileIndex } from "../constants";
+import { metadata as rows } from "../components/Map";
+
+export function endsUpInValidPosition(currentPosition, moves) {
+    // calculate where player would end after move
+    const finalPosition = calculateFinalPosition(currentPosition, moves);
+    
+    // Detect if player hits edge of board
+    if (
+        finalPosition.rowIndex === -1 ||
+        finalPosition.tileIndex === minTileIndex - 1 ||
+        finalPosition.tileIndex === maxTileIndex + 1
+    ) {
+        // Move is invalid, ignore command
+        return false;
+    }
+
+    // Detect if tree is obstacle
+    const finalRow = rows[finalPosition.rowIndex -1];
+    if (
+        finalRow &&
+        finalRow.type === "forest" &&
+        finalRow.trees.some(
+            (tree)  => tree.tileIndex === finalPosition.tileIndex
+        )
+    ) {
+        // Move is invalid, ignore command.
+    }
+
+    return true;
+}
